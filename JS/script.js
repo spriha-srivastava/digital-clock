52759a6bf80f0c4d38e31ce9e4b4927ea0ab1cd7
Spriha Srivastava <srivastavaspriha18@gmail.com>
2023-06-20 11:24:47 +0530

    init commit

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+function updateTime(){
+    var dateTime = new Date();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
+    var seconds = dateTime.getSeconds();
+    var am_or_pm = document.getElementById("am-or-pm");
+
+    if(hours >= 12){
+        am_or_pm.innerHTML = "PM";
+    }else{
+        am_or_pm.innerHTML = "AM";
+    }
+
+    if(hours > 12){
+        hours = hours - 12;
+    }
+
+     document.getElementById("hours").innerHTML = hours;
+     document.getElementById("minutes").innerHTML = minutes;
+     document.getElementById("seconds").innerHTML = seconds;
+}
+
+var i = 0
+
+
+
+setInterval(updateTime, 1000);
